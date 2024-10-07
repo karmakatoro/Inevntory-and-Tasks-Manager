e6affa9a103577869241edc74314cb493ec3fc6f
karmakatoro <karmakatoro@gmail.com>
2024-10-07 07:35:08 +0200

    auth basics routes created even for dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.welcome');
+Route::middleware('auth')->group(function () {
+    Route::prefix('auth')->controller(AuthController::class)->group(function () {
+        Route::get('/', 'index')->name('login');
+        Route::get('/forgot', 'forgot')->name('auth.forgot');
+        Route::get('/logout', 'AuthController@logout')->name('auth.logout');
+        Route::post('/get-connected', 'get_connected')->name('auth.get-connected');
+        Route::get('/get-logged-out', 'get_logged_out')->name('auth.get-logged-out');
+    });
+    Route::prefix('dashboard')->controller(DashboardController::class)->group(function () {
+        Route::get('/sales', 'index')->name('dashboard.sales');
+        Route::get('/analytics', 'analytics')->name('dashboard.analytics');
+    });
 });
