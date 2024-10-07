852d0cefa10a26e63aad317f4b9d83c983838f1e
karmakatoro <karmakatoro@gmail.com>
2024-10-07 09:36:54 +0200

    views created and updates for users, settings, dahsboard, and logout

---
+++ resources/views/pages/settings/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.base')
+
+@section('title', 'Settings - ' . env('APP_NAME'))
+
+@section('content')
+
+@endsection
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.base')
+
+@section('title', 'Users - ' . env('APP_NAME'))
+
+@section('content')
+
+@endsection
