a65ab66ff8c37150896529047d1e569e50f86347
karmakatoro <karmakatoro@gmail.com>
2024-10-07 08:08:10 +0200

    user database migration setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 50);
+            $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('photo', 50)->nullable(false);
+            $table->enum('gender', ['m', 'f'])->default('m');
+            $table->enum('type', ['admin', 'user'])->default('user');
+            $table->enum('accred', ['1', '2', '3'])->default('3');
+            $table->enum('status', ['on', 'off'])->default('off');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
