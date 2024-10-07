37765f745ae5f0bc88aa304552e41f535a4293a4
karmakatoro <karmakatoro@gmail.com>
2024-10-07 08:08:44 +0200

    user seeder setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,15 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Karma Katoro',
+            'email' => 'karmakatoro@gmail.com',
+            'password' => Hash::make('passwordd'),
+            'gender' => 'm',
+            'avatar' => 'avatar-male.png',
+            'accred' => '1',
+            'type' => 'admin',
+            'status' => 'on',
+        ]);
     }
 }
